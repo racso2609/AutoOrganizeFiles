82f84f27f213dfd89445babff4676460208556a7
Oscar Lugo <oscaremiliolugo@gmail.com>
2022-03-26 20:50:08 +0000

    timestamp config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,12 @@
 const config = require('./config.json');
+
+const timestamp = config.timeStamp || '*/30 * * * * *';
 module.exports = {
   apps: [
     {
       script: 'index.js',
       name: 'fileOrganizer',
-      cron_restart: config.timeStamp,
+      cron_restart: timestamp,
     },
   ],
 };
